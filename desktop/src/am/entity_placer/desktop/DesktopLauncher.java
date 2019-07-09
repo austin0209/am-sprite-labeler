c3dbbd2de018394cb6d13883d4957f924819c0da
austin0209 <austinrandy0209@gmail.com>
2019-07-08 22:24:12 -0500

    initial commit

---
+++ desktop/src/am/entity_placer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package am.entity_placer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import am.entity_placer.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
